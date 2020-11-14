70e5b1ab3443a0f0f84e23034837f00598da9a54
Thapus Simon <thapus95@gmail.com>
2020-11-14 19:59:19 +0530

    Dummy Customer class created

---
+++ IMS/src/Customer.java
@@ -0,0 +1,29 @@
+public class Customer {
+    //This is a dummy Customer class, This will be modified further
+    int id;
+    String contactName;
+    String contactPhone;
+    String contactEmail;
+    String addressState;
+    String addressCity;
+    String addressStreet;
+
+    int noOfTransactions;
+
+    public int getTransactions() {
+        return noOfTransactions;
+    }
+
+    public float calculateDiscount() {
+        return (float) (noOfTransactions * 0.1);
+    }
+
+    public String getAddress() {
+        return addressStreet + ", " + addressCity + ", " + addressState;
+    }
+
+    public String getContact() {
+        return contactName + ", " + contactPhone + ", " + contactEmail;
+    }
+
+}
